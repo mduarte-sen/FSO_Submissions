18ddc29307f79b88abaca8f9ed76904dfb16fd18
mduarte-sen <maxium97@gmail.com>
2023-01-09 22:04:23 -0300

    exercises 1.3, 1.4 & 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-const Content = ... {
+const Content = parts => {
   return (
     <div>
       <Part ... />
@@ -10,13 +10,23 @@ const Content = ... {
 
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1= 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'state of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     // <div>
@@ -37,8 +47,8 @@ const App = () => {
 
     <div>
       <Header course={course} />
-      <Content ... />
-      <Total ... />
+      <Content parts = {parts} />
+      <Total parts = {parts} />
     </div>
   )
 }
