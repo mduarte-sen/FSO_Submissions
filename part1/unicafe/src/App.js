6620a62cc738755bb927b70ecf1eab14e94f0e00
mduarte-sen <maxium97@gmail.com>
2023-01-22 20:08:12 -0300

    excercises 1.12 to 1.14

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -39,9 +39,9 @@ const Statistics = ({good, neutral, bad}) => {
 
 
 function App() {
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
 
   return (
